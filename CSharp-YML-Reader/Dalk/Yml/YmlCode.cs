ce53f435563cc2e853223d3bb6f02c953d660d34
Dalk YouTube <dalkyoutube@balk-mail.de>
2020-11-19 15:54:41 +0100

    added getbool,getint,getstring

--- CSharp-YML-Reader/Dalk/Yml/YmlCode.cs
+++ CSharp-YML-Reader/Dalk/Yml/YmlCode.cs
@@ -20,9 +20,42 @@ namespace Dalk.Yml
         }
         public string GetString(string name)
         {
-            string s = "";
-
-            return s;
+            string res = "";
+            string[] con = code;
+            foreach (string s in con)
+            {
+                if (s.StartsWith(name + ": "))
+                {
+                    res = s.Remove(0, name.Length + 2);
+                }
+            }
+            return res;
+        }
+        public bool GetBool(string name)
+        {
+            string res = "false";
+            string[] con = code;
+            foreach (string s in con)
+            {
+                if (s.StartsWith(name + ": "))
+                {
+                    res = s.Remove(0, name.Length + 2);
+                }
+            }
+            return Convert.ToBoolean(res);
+        }
+        public int GetInt(string name)
+        {
+            string res = "0";
+            string[] con = code;
+            foreach (string s in con)
+            {
+                if (s.StartsWith(name + ": "))
+                {
+                    res = s.Remove(0, name.Length + 2);
+                }
+            }
+            return Convert.ToInt32(res);
         }
         public void Load(YmlFile file)
         {
